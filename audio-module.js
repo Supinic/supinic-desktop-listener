93212583f6dfa5ed117cb175da3377cee40f129d
Supinic <supinic@protonmail.com>
2021-06-17 18:00:27 +0200

    playsounds redone - uses VLC instead of a speaker module

--- audio-module.js
+++ audio-module.js
@@ -1,18 +1,35 @@
 module.exports = (function () {
 	const got = require("got");
-	const Player = require("node-wav-player");
+	// const Player = require("node-wav-player");
 	const { promisify } = require("util");
 	const exec = promisify(require('child_process').exec);
 
-	const getDuration = require("get-mp3-duration");	
+	const getDuration = require("get-mp3-duration");
 
 	return class AudioPlayer {
 		async play (filename) {
 			try {
-				await Player.play({
-					path: `.\\playsounds\\${filename}`
-				});
-				
+				const params = [
+					"vlc",
+					// "--audio-filter normvol",
+					// "--norm-max-level=1.500000",
+					"-I dummy",
+					"--no-volume-save",
+					//"--waveout-volume=0.05",
+					//"--waveout-volume=" + volume,
+					"--gain=0.375",
+					//"--gain=8",
+					"--play-and-exit",
+					"--no-one-instance",
+					`.\\playsounds\\${filename}`
+				];
+
+				await exec(params.join(" "));
+
+				// await Player.play({
+				// 	path: `.\\playsounds\\${filename}`
+				// });
+
 				return {
 					success: true
 				};
@@ -24,24 +41,24 @@ module.exports = (function () {
 				};
 			}
 		}
-				
-		async playFromURL (list, volume, limit) {			
+
+		async playFromURL (list, volume, limit) {
 			const resp = await Promise.all(list.map(obj => got({
 				url: obj.url,
 				searchParams: obj.searchParams || "",
 				responseType: "buffer"
 			})));
-			
+
 			const duration = resp.reduce((acc, cur) => {
 				const subDuration = getDuration(cur.body);
-				return acc += subDuration;
-			}, 0);			
-			
+				return (acc + subDuration);
+			}, 0);
+
 			if (duration > limit) {
 				console.debug({duration, limit});
 				return false;
 			}
-		
+
 			const stringURLs = list.map(i => `"${i.url}?${i.searchParams}"`).join(" ");
 			const params = [
 				"vlc",
@@ -55,12 +72,12 @@ module.exports = (function () {
 				//"--gain=8",
 				"--play-and-exit",
 				"--no-one-instance",
-				stringURLs			
+				stringURLs
 			];
-			
+
 			console.debug(stringURLs);
-			
-			await exec(params.join(" "));			
+
+			await exec(params.join(" "));
 			return true;
 		}
 	}
